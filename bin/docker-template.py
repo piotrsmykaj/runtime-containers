29e400c201e36d3e83dd7714b23e383381507d44
Matthieu Nogatchewsky <matthieu.nogatchewsky@continuous.team>
2019-07-11 14:56:42 +0200

    Stable state

--- bin/docker-template.py
+++ bin/docker-template.py
@@ -8,14 +8,14 @@ import yaml
 
 
 variables = {}
-parser = argparse.ArgumentParser(description='Runtime containers generator')
-parser.add_argument('cmd', nargs='?', type=str, help='Command')
-parser.add_argument('runtime', nargs='?', type=str, help='Runtime')
-parser.add_argument('--version', nargs='*', default='all',
-                    type=str, help='List of versions to compile')
-parser.add_argument('--clean', dest='clean',
-                    action='store_const', const=True, default=False)
-args = parser.parse_args()
+# parser = argparse.ArgumentParser(description='Runtime containers generator')
+# parser.add_argument('cmd', nargs='?', type=str, help='Command')
+# parser.add_argument('runtime', nargs='?', type=str, help='Runtime')
+# parser.add_argument('--version', nargs='*', default='all',
+#                     type=str, help='List of versions to compile')
+# parser.add_argument('--clean', dest='clean',
+#                     action='store_const', const=True, default=False)
+# args = parser.parse_args()
 
 
 def leaf(path):
